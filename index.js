a1d8eb7fec4f47464341b1d6b29d2544d46cf6eb
inqilobchi <hieojody@gmail.com>
2026-02-03 11:27:27 +0500

    Update index.js

--- index.js
+++ index.js
@@ -123,7 +123,7 @@ async function getSubscriptionMessage() {
   const SUPPORT_BOT_LINK = 'https://t.me/TurfaSeenBot?start=user19';
   const SUPPORT_BOT_TITILE = 'Turfa Seen | Rasmiy🤖';
   const PRIVATE_CHANNEL_LINK = 'https://t.me/+ye-1jYtfdsIzODUy';
-  const PRIVATE_CHANNEL_TITILE = '🟢 — O'zimcha';
+  const PRIVATE_CHANNEL_TITILE = `🟢 — O'zimcha`;
   buttons.push([{ text: `${SUPPORT_BOT_TITILE}`, url: SUPPORT_BOT_LINK }]);  
   buttons.push([{ text: `${PRIVATE_CHANNEL_TITILE}`, url: PRIVATE_CHANNEL_LINK }]);  
   buttons.push([{ text: '✅ Obuna bo‘ldim', callback_data: 'check_subscription' }]);
